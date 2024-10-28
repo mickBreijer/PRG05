0f0248f475dd3381ad4a42b29d8bf1d8ff5220f8
mickb <mickbreijer@outlook.com>
2024-10-28 13:03:59 +0100

    Changed the create to grey out already selected players for the same position

--- resources/views/teams/create.blade.php
+++ resources/views/teams/create.blade.php
@@ -1,5 +1,5 @@
 <x-layout><br>
-    <form action="{{ route('teams.store') }}" method="POST">
+    <form action="{{ route('teams.store') }}" method="POST" id="team-form">
         @csrf
         <div>
             <label for="name">Maak je Team</label>
@@ -20,22 +20,22 @@
             @endif
 
             <h4>Keeper</h4>
-            <select name="players[keeper]" required>
+            <select name="players[keeper]" required onchange="updateOptions(this)">
                 <option value="">Kies een keeper</option>
                 @foreach($players as $player)
                     @if($player->position == 'Keeper')
-                    <option value="{{ $player->id }}">{{ $player->name }}</option>
+                        <option value="{{ $player->id }}">{{ $player->name }}</option>
                     @endif
                 @endforeach
             </select><br>
 
             <h4>Verdedigers</h4>
             @for ($i = 1; $i <= 4; $i++)
-                <select name="players[verdediger][]" required>
+                <select name="players[verdediger][]" required onchange="updateOptions(this)">
                     <option value="">Kies een verdediger</option>
                     @foreach($players as $player)
                         @if($player->position == 'Verdediger')
-                        <option value="{{ $player->id }}">{{ $player->name }}</option>
+                            <option value="{{ $player->id }}">{{ $player->name }}</option>
                         @endif
                     @endforeach
                 </select><br>
@@ -43,11 +43,11 @@
 
             <h4>Middenvelders</h4>
             @for ($i = 1; $i <= 3; $i++)
-                <select name="players[middenvelder][]" required>
+                <select name="players[middenvelder][]" required onchange="updateOptions(this)">
                     <option value="">Kies een middenvelder</option>
                     @foreach($players as $player)
                         @if($player->position == 'Middenvelder')
-                        <option value="{{ $player->id }}">{{ $player->name }}</option>
+                            <option value="{{ $player->id }}">{{ $player->name }}</option>
                         @endif
                     @endforeach
                 </select><br>
@@ -55,11 +55,11 @@
 
             <h4>Aanvallers</h4>
             @for ($i = 1; $i <= 3; $i++)
-                <select name="players[aanvaller][]" required>
+                <select name="players[aanvaller][]" required onchange="updateOptions(this)">
                     <option value="">Kies een aanvaller</option>
                     @foreach($players as $player)
                         @if($player->position == 'Aanvaller')
-                        <option value="{{ $player->id }}">{{ $player->name }}</option>
+                            <option value="{{ $player->id }}">{{ $player->name }}</option>
                         @endif
                     @endforeach
                 </select><br>
@@ -68,4 +68,23 @@
 
         <button type="submit">Publiceer</button>
     </form>
+
+    <script>
+        function updateOptions(selectElement) {
+            const selectedValues = Array.from(document.querySelectorAll('select'))
+                .map(select => select.value)
+                .filter(value => value);
+
+            document.querySelectorAll('select').forEach(select => {
+                const options = select.querySelectorAll('option');
+                options.forEach(option => {
+                    if (selectedValues.includes(option.value) && option.value !== select.value) {
+                        option.disabled = true;
+                    } else {
+                        option.disabled = false;
+                    }
+                });
+            });
+        }
+    </script>
 </x-layout>
